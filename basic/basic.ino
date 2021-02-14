f142d0e69863baafabfd7aea84e979d1952ccfd5
olive <01ive@free.fr>
2021-02-14 18:26:30 +0100

    First proto

--- basic/basic.ino
+++ basic/basic.ino
@@ -1,36 +1,65 @@
 #include "buzzer.h"
 #include "pressure_sensor.h"
 
+//#define DEBUG 1
+
+const float pressure_sensibility = 1;
+
 Pressure_Sensor pressure;
 Buzzer buzzer;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.print("\n\n===========================================================\n");
-  Serial.print("Hello !!!\n\n");
+float current_pressure = 0;
+float previous_pressure = 0;
 
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  #ifdef DEBUG
+    // init serial DEBUG
+    Serial.begin(9600);
+    Serial.print("\n\n===========================================================\n");
+    Serial.print("Hello !!!\n\n");
+  #endif
+
+  // Setup app
   pressure.start_up();
+  previous_pressure = pressure.readPressure();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  buzzer.down();
-  Serial.print(F("Temperature = "));
-  Serial.print(pressure.readTemperature());
-  Serial.println(" *C");
-  Serial.print(F("Pressure = "));
-  Serial.print(pressure.readPressure());
-  Serial.println(" Pa");
-  Serial.print(F("Approx altitude = "));
-  Serial.print(pressure.readAltitude(1013.25)); /* Adjusted to local forecast! */
-  Serial.println(" m");
+  float delta_pressure;
+  
+  current_pressure = pressure.readPressure();
+  delta_pressure = current_pressure - previous_pressure;
 
-  Serial.println();
-  delay(3000);
-  digitalWrite(LED_BUILTIN, LOW);
-  buzzer.up();
-  delay(1000);
+  #ifdef DEBUG
+    Serial.print(F("Current pressure = "));
+    Serial.print(current_pressure);
+    Serial.println(" Pa");
+    Serial.println();
+    Serial.print(F("Previous pressure = "));
+    Serial.print(previous_pressure);
+    Serial.println(" Pa");
+    Serial.print(F("Delta pressure = "));
+    Serial.print(delta_pressure);
+    Serial.println(" Pa");
+    Serial.println();
+  #endif
+
+  if (delta_pressure >= ((float)pressure_sensibility)) {
+    #ifdef DEBUG
+      Serial.print(F("Up !!!!!\n"));
+    #else
+        buzzer.down();
+    #endif
+  } 
+  else if (delta_pressure < ((float)-pressure_sensibility)){
+    #ifdef DEBUG
+      Serial.print(F("Down !!!!!\n"));
+    #else
+      buzzer.up();
+    #endif
+  }
+
+  previous_pressure = current_pressure;
+  
+  delay(500);
 }
