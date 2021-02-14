eea321e14d1f84a3f1b4c9be1c2d69de0302b99d
olive <01ive@free.fr>
2021-02-14 10:47:35 +0100

    First buz

--- basic/basic.ino
+++ basic/basic.ino
@@ -1,3 +1,7 @@
+#include "pitches.h"
+
+const int buzPin  = 2;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
@@ -6,7 +10,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  tone(buzPin, NOTE_C5, 500);
+  delay(3000);
   digitalWrite(LED_BUILTIN, LOW);
+  tone(buzPin, NOTE_A6, 500);
   delay(1000);
 }
