5e07c4a225374fc145a481f07fcab4c02890b8b6
olive <01ive@free.fr>
2021-04-02 17:41:59 +0200

    First test temp ok but no pressure

--- vario/pressure_sensor.h
+++ vario/pressure_sensor.h
@@ -1,4 +1,5 @@
 #include <Adafruit_BMP280.h>
+#include <Wire.h>
 
 class Pressure_Sensor : public Adafruit_BMP280 {
     public:
@@ -7,5 +8,6 @@ class Pressure_Sensor : public Adafruit_BMP280 {
             PRESSURE_SENSOR_FAIL = 1
         }t_return_code;
 
+        Pressure_Sensor::Pressure_Sensor(TwoWire *);
         Pressure_Sensor::t_return_code start_up();
 };
